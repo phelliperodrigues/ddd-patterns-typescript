eea3ba682eda3c316ce4027077d834fa5b2e9653
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-03-22 22:32:14 -0300

    Change Anemic Entity to Entity with business rules

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,36 +1,26 @@
 class Customer {
+
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean = true;
 
     constructor(id: string, name: string, address: string){
         this._id = id;
         this._address = address;
         this._name = name;
     }
-    
-    get id(): string {
-        return this._id;
-    }
 
-    get name(): string {
-        return this._name;
-    }
-
-    get address(): string {
-        return this._address;
+    changeName(name: string) {
+        this._name = name;
     }
     
-    set address(address: string) {
-        this._address =address;
+    activate() {
+        this._active = true;
     }
 
-    set id(id: string) {
-        this._id =id;
-    }
-
-    set name(name: string) {
-        this._name =name;
+    deactivate() {
+        this._active = false;
     }
     
 }
