0e5f39637b4129127f6660c6cbb6e36d4a36d258
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-05-04 22:03:08 -0300

    Implement principle self validation

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,21 +1,35 @@
 class Customer {
 
     _id: string;
-    _name: string;
-    _address: string;
-    _active: boolean = true;
+    _name: string = "";
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string){
+    constructor(id: string, name: string){
         this._id = id;
-        this._address = address;
         this._name = name;
+        this.validate();
+    }
+
+    validate() {
+        if (this._name.length === 0){
+            throw new Error("Name is required");
+        }
+
+        if (this._id.length === 0){
+            throw new Error("Id is required");
+        }
     }
 
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
     
     activate() {
+        if (this._address.length === 0){
+            throw new Error("Address is mandatory to activate  a customer");
+        }
         this._active = true;
     }
 
@@ -24,3 +38,5 @@ class Customer {
     }
     
 }
+
+let customer = new Customer("123", "Phellipe");
