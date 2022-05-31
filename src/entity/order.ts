8075db08d8bf0614dc42bdb4c9fafc45ef9a86df
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-05-30 22:25:17 -0300

    Configurate env from test unit with jest

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -3,7 +3,7 @@ import OrderItem from "./order_item";
 export default class Order {
     _id: string;
     _customerId: string;
-    _items: OrderItem[] = [];
+    _items: OrderItem[];
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
