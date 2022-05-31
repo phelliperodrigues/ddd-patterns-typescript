e076f812d30d9cbf4864193bcbc01d7c11eba8f8
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-05-31 20:54:43 -0300

    Configurate tsc from verify sintax typescript

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import Order from "./entity/order";
 import OrderItem from "./entity/order_item";
 
 // Customer Agregate
-let customer = new Customer("123", "Phellipe Rodrigues");
+const customer = new Customer("123", "Phellipe Rodrigues");
 const address = new Address("Rua 1", 2, "12345-000", "Cidade");
 customer.Adderss = address;
 customer.activate();
 
 // Order Agregate
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("1", "Item 2", 15);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+const item2 = new OrderItem("1", "Item 2", 15, "p2", 3);
 const order = new Order("1", "123", [item1, item2]);
